c12a2edefdfd2d4c76bf652e388a8486c3b05670
Gronik64 <golegni@yandex.ru>
2021-08-24 18:57:48 +0300

    Очередная функция в mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,6 +2,7 @@ const deadObject = document.getElementById('dead');
 let dead = Number(deadObject.textContent);
 const lostObject = document.getElementById('lost');
 let lost = Number(lostObject.textContent);
+let text;
 for(let i = 1; i < 10; i++) {
     let numberId ='hole' + i;
     let hole = document.getElementById(`${numberId}`);
@@ -12,33 +13,27 @@ function getHole() {
     const ids = document.getElementById(this.id);
     if(ids.classList.contains( 'hole_has-mole' )){
         dead += 1;
-        outputVictory();
+        text = 'Ура!!! Победа!!!'
+        if(dead == 10){
+        outputResalt(text);
+        } else {
+            deadObject.textContent = dead;
+        }  
     }else{
         lost += 1;
-        outputLoss();
+        text = 'Увы!! Вы проиграли!'
+        if(lost == 5){
+        outputResalt(text);
+        } else {
+            lostObject.textContent = lost;
+        } 
     }
 }
 
-function outputVictory(){
-    if(dead == 10){
-        alert('Ура!!! Победа!!!');
+function outputResalt(text) {
+    alert(text);
         lost = 0;
         dead = 0;
         deadObject.textContent = 0;
         lostObject.textContent = 0;
-    } else {
-        deadObject.textContent = dead;
-    }    
-}
-
-function outputLoss() {
-    if(lost == 5){
-        alert('Увы!! Вы приграли!')
-        lost = 0;
-        dead = 0;
-        deadObject.textContent = 0;
-        lostObject.textContent = 0;
-    } else {
-        lostObject.textContent = lost;
-    } 
 }
