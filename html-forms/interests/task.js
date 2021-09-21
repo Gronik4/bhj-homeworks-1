202322e73cbc763c6b07bbee7353b3fc69dc95fd
Gronik64 <golegni@yandex.ru>
2021-09-21 19:18:25 +0300

    Доработал задачу interest

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,50 +1,59 @@
 const interests = document.querySelector('ul').children; //<li class="interest">
 for(let item of interests) {
-    item.querySelector('label').addEventListener('click', chekMain);
+    item.querySelector(' input').addEventListener('click', chekMain);
     let labels_1 = item.querySelector('ul').children;
     for(let i of labels_1) {
-        i.querySelector('label').addEventListener('click', chekMain);
+        i.querySelector('input').addEventListener('click', chekChildren);
     }
 }
 
 function chekMain(event) {
-    let et = event.target;
-    if(et.hasAttribute("checked")) {
-        et.removeAttribute("checked");
-    } else {
-        et.setAttribute("checked", "checked");
-    }
-    if(!et.closest('ul').classList.contains('interests')) { // Не <ul class="interest interests_active">
-        let inputs = et.closest('label').nextElementSibling.children; //<ul class="interests interests_active"><li class="interest">
-        if(et.hasAttribute('checked')) {
-            for(let i of inputs) {
-                i.querySelector('input').setAttribute("checked", "checked");//Ставим чеки на глубокие input-ы
-            }
-        }else{
-            for(let i of inputs) {
-                i.querySelector('input').removeAttribute("checked"); //Убираем чеки с глубоких input-ов
-            }
-        }
-    } else {
-        let counter = 0;
-        let active = et.closest('.interests_active');
-        let mainInput = active.previousElementSibling.querySelector('.interest__check');
-        let interestObject = active.querySelectorAll('.interest');
-        for(let j of interestObject) {
-            if(j.querySelector('input').hasAttribute("checked")) {counter += 1;}
-        }
-        switch(counter) {
-            case 2:
-                mainInput.indeterminate = false;
-                mainInput.setAttribute("checked", "checked");
-            break;
-            case 1:
-                mainInput.indeterminate = true;
-            break;
-            default:
-                mainInput.indeterminate = false;
-                mainInput.removeAttribute("checked");
-        }
-        
-    }
+   let its = checkeder(event);
+   let inputs = its.closest('label').nextElementSibling.children;
+   if(its.checked){
+      for(let i of inputs) {
+         i.querySelector('input').checked = true;//Ставим чеки на глубокие input-ы
+      } 
+   } else {
+      for(let i of inputs){
+         i.querySelector('input').checked = false;
+      }
+   its.checked = false;
+   }
+}
+
+function chekChildren(event) {
+   let itss = checkeder(event);   
+   let counter = 0; 
+   let active = itss.closest('.interests_active');
+   let mainInput = active.previousElementSibling.querySelector('input');
+   let inputs = active.querySelectorAll('input');
+   for(let j of inputs) {
+      if(j.checked) {
+         counter += 1;
+       }
+   }
+   switch(counter) {
+      case 2:
+         mainInput.indeterminate = false;
+         mainInput.checked= true;
+      break;
+      case 1:
+         mainInput.indeterminate = true;
+         mainInput.checked = false;
+      break;
+      default:
+         mainInput.indeterminate = false;
+         mainInput.checked = false;
+   }
+}
+
+function checkeder(e) {
+   let rezalt = e.target;
+   if(rezalt.hasAttribute("checked")) {
+      rezalt.removeAttribute("checked");
+   } else {
+      rezalt.setAttribute("checked", "checked");
+   }
+   return rezalt;
 }
