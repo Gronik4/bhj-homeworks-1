c412b6653892bd87ce85f48a0ae287ed0d7ab005
Gronik64 <golegni@yandex.ru>
2021-09-23 19:14:07 +0300

    Решение задачи tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,22 @@
+let placeClick = document.querySelectorAll('.has-tooltip');
+for(let item of placeClick) {
+   item.addEventListener('click', handlerHint);
+}
+
+function handlerHint(event) {
+   event.preventDefault();
+   let hint = event.target.title;
+   let placeE = event.target.closest('a');
+   let placeHints = document.querySelectorAll('.tooltip')
+   for(let del of placeHints) {
+      del.remove();
+   }
+   let div = document.createElement('div');
+   div.className = 'tooltip';
+   div.style = 'left: 30%; top: 24%;';
+   div.setAttribute("data-position", "left");
+   div.innerHTML = `${hint}`;
+   div.classList.add('tooltip_active');
+   placeE.insertAdjacentHTML('afterend', div.outerHTML);
+   setTimeout(() => this.nextSibling.remove(), 1600);
+}
