9e85156fb8bd453b8fef43a2b9e51804e59b0b81
Gronik64 <golegni@yandex.ru>
2021-09-29 22:07:40 +0300

    Решение задачи auth

---
+++ client-state/auth/task.js
@@ -0,0 +1,43 @@
+const signin = document.getElementById('signin');
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+let userId = document.getElementById('user_id');
+
+let ajax = new XMLHttpRequest(); 
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let formData = new FormData(form);
+    ajax.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+    ajax.responseType = "json";
+    ajax.send(formData);
+}
+
+ajax.onload = function() {
+    if(ajax.status != 200) {
+        alert(`Ошибка ${ajax.status} : ${ajax.statusText}`);
+    } else {
+        if(!ajax.response.success) {
+            alert('Неверный логин/пароль');
+        } else {
+            let idA = ajax.response.user_id;
+            userId.textContent = idA;
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+            localStorage.usaid = `${idA}`;
+        }
+    }
+}
+window.onload = function() {
+    let controls = form.querySelectorAll('input');
+    for(let item of controls) {
+        item.value = "";
+    }
+    let keep = localStorage.usaid;
+    if(keep) {
+        userId.textContent = keep;
+        signin.classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        localStorage.removeItem('usaid');
+    } 
+}
